9d6b1dd709c87fee33b9d9f9ad62b7e81f219dd0
Fabian Krautgasser <fkrautgasser@posteo.org>
2019-01-07 11:27:17 +0100

    cleanup + example firebase auth file

---
+++ vertical-timeline-master/js/example_firebase_auth.js
@@ -0,0 +1,7 @@
+let firebaseAuth = {
+  apiKey: 'yourAPIKey',
+  authDomain: 'YourProject.firebaseapp.com',
+  databaseURL: 'https://YourProject.firebaseio.com',
+  projectId: 'YourProjectID',
+  storageBucket: 'YourProject.appspot.com'
+}
